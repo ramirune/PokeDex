c2663dbdd23f6cc3ad8152eff3a80bdba908ee96
ramirune <timpiga3@hotmail.com>
2021-09-24 14:15:58 +0200

    Update scripts.js
    created variable pokemonList and
    added three pokemon objects to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood='Thai Food';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "kakuna", height: 0.6, type: ["bug", "poison"]},
+  {name: "gloom", height: 0.8, type: ["grass", "poison"]},
+  {name: "golem", height: 1.4, type: ["rock","ground"]}
+];
