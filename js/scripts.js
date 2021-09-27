35d9ea5eafe06b235ba15c5aa7f10e6109c605e2
ramirune <timpiga3@hotmail.com>
2021-09-27 14:20:50 +0200

    Update scripts.js
    create for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
+//create pokemon list
 let pokemonList = [
   {name: "kakuna", height: 0.6, type: ["bug", "poison"]},
   {name: "gloom", height: 0.8, type: ["grass", "poison"]},
   {name: "golem", height: 1.4, type: ["rock","ground"]}
 ];
+
+for (let i=0; i<pokemonList.length; i++){
+  document.write(pokemonList[i].name+ "(Height:"+pokemonList[i].height+")");
+  if (pokemonList[i].height>1){
+    document.write("-Wow,that\'s BIG!");
+  }
+  document.write("<br>");
+}
