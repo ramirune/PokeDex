c5d5ab4868c20cebabaa3dd11c379adb46c2e40b
ramirune <timpiga3@hotmail.com>
2021-09-28 17:29:15 +0200

    Update scripts.js
    wrap the pokemonList array in an IIFE.
    And update the loop code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
-//create pokemon list
-let pokemonList = [
-  {name: "kakuna", height: 0.6, type: ["bug", "poison"]},
-  {name: "gloom", height: 0.8, type: ["grass", "poison"]},
-  {name: "golem", height: 1.4, type: ["rock","ground"]}
-];
+let pokemonRepository = (function () {
+  //create pokemon list
+  let pokemonList = [
+    {name: "kakuna", height: 0.6, type: ["bug", "poison"]},
+    {name: "gloom", height: 0.8, type: ["grass", "poison"]},
+    {name: "golem", height: 1.4, type: ["rock","ground"]}
+  ];
 
-// printArrayDetails by using for loop
-// function printArrayDetails(){
-//   for (let i=0; i<pokemonList.length; i++){
-//     document.write(pokemonList[i].name+ "(Height:"+pokemonList[i].height+")");
-//     if (pokemonList[i].height>1){
-//       document.write("-Wow,that\'s BIG!"); //add code to highlight big pokemon in the list
-//     }
-//     document.write("<br>"); //new line
-//   }
-// }
+  //Add item function to the pokemonList array
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+ //return item function
+  function getAll() {
+    return pokemonList;
+  }
+//return values
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
-//forEach function to printout he list
+// create forEach function to printout he list
 function myPokemonList(list) {
   document.write(list.name + ' Height: ' + list.height);
   if (list.height > 1) {
@@ -24,5 +29,4 @@ function myPokemonList(list) {
   }
   document.write('<br>');
 }
-
-pokemonList.forEach(myPokemonList);
+pokemonRepository.getAll().forEach(myPokemonList);
