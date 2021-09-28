8cd49dc9312222c54cb359b28f57f1c317a3c3ab
ramirune <timpiga3@hotmail.com>
2021-09-28 16:50:21 +0200

    Update scripts.js
    change from using "for" loop to "forEach function" to printout the pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,24 @@ let pokemonList = [
   {name: "golem", height: 1.4, type: ["rock","ground"]}
 ];
 
-for (let i=0; i<pokemonList.length; i++){
-  document.write(pokemonList[i].name+ "(Height:"+pokemonList[i].height+")");
-  if (pokemonList[i].height>1){
-    document.write("-Wow,that\'s BIG!"); //add code to highlight big pokemon in the list
+// printArrayDetails by using for loop
+// function printArrayDetails(){
+//   for (let i=0; i<pokemonList.length; i++){
+//     document.write(pokemonList[i].name+ "(Height:"+pokemonList[i].height+")");
+//     if (pokemonList[i].height>1){
+//       document.write("-Wow,that\'s BIG!"); //add code to highlight big pokemon in the list
+//     }
+//     document.write("<br>"); //new line
+//   }
+// }
+
+//forEach function to printout he list
+function myPokemonList(list) {
+  document.write(list.name + ' Height: ' + list.height);
+  if (list.height > 1) {
+    document.write('-Wow,that\'s BIG!'); //create highlight when the pokemon size is bigger than 1
   }
-  document.write("<br>"); //new line
+  document.write('<br>');
 }
+
+pokemonList.forEach(myPokemonList);
