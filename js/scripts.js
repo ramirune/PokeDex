29b583a1456ac01ae0507c46c5e528ce31223897
ramirune <timpiga3@hotmail.com>
2021-09-29 20:57:34 +0200

    Update scripts.js
    add Add function
    add addListItem function
    add a pokemon on the list
     adjust forEach loop function according to the addListItem function

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,65 @@
 let pokemonRepository = (function () {
   //create pokemon list
   let pokemonList = [
-    {name: "kakuna", height: 0.6, type: ["bug", "poison"]},
-    {name: "gloom", height: 0.8, type: ["grass", "poison"]},
-    {name: "golem", height: 1.4, type: ["rock","ground"]}
+    {name: "Kakuna", height: 0.6, type: ["bug", "poison"]},
+    {name: "Gloom", height: 0.8, type: ["grass", "poison"]},
+    {name: "Golem", height: 1.4, type: ["rock","ground"]}
   ];
 
   //Add item function to the pokemonList array
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if(
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "type" in pokemon
+    ){
+      pokemonList.push(pokemon);
+    }else{
+      console.log("pokemon is not correct");
+    }
   }
- //return item function
+  //return item function
   function getAll() {
     return pokemonList;
   }
-//return values
+
+
+  function addListItem(pokemon){
+    let pokeList = document.querySelector(".pokemon-list"); //select pokemon-list class from html
+    let listItem = document.createElement("li"); // create list items
+
+    let button = document.createElement("button"); // create button
+    button.innerText = pokemon.name; //add pokemon name from the pokemonList to each button
+    button.classList.add("button1"); //add button styles
+    button.addEventListener('click', function(){ //add event to button
+      showDetails(pokemon);
+    });
+
+    listItem.appendChild(button);
+    pokeList.appendChild(listItem);
+  }
+
+  //create function to show clicked pokemon's details
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  //return values
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-// create forEach function to printout he list
-function myPokemonList(list) {
-  document.write(list.name + ' Height: ' + list.height);
-  if (list.height > 1) {
-    document.write('-Wow,that\'s BIG!'); //create highlight when the pokemon size is bigger than 1
-  }
-  document.write('<br>');
-}
-pokemonRepository.getAll().forEach(myPokemonList);
+//add a pokemon on the list
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:"Seal", height:1.1, type:["water"]});
+console.log(pokemonRepository.getAll());
+
+// create forEach function to printout the list
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
