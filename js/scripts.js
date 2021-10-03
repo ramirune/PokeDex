af2fc612c015a6fe4d16e8915f2a2db10070eaf0
ramirune <timpiga3@hotmail.com>
2021-10-03 23:51:07 +0200

    Update scripts.js
    add function to show and hide modal container. then show the pokemon's details on the container

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ let pokemonRepository = (function () {
   // ];
 
   let pokemonList = []; //create an empty array for pokemonList
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150"; //url to fetch the data
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150"; //url to fetch the data from API
 
   //Add item function to the pokemonList array
   function add(pokemon) {
@@ -24,7 +24,6 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
-
 
   function addListItem(pokemon){
     let pokeList = document.querySelector(".pokemon-list"); //select pokemon-list class from html
@@ -43,12 +42,72 @@ let pokemonRepository = (function () {
 
   //create function to show clicked pokemon's details
   function showDetails(pokemon) {
-  loadDetails(pokemon).then(function () {
-    console.log(pokemon);
-  });
-}
+    loadDetails(pokemon).then(function () {
+      showModal(pokemon);
+    });
+  }
 
-// create load function to fetch data fron API
+  //show modal function
+  let modalContainer = document.querySelector('#modal-container');
+
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'Height:' + pokemon.height;
+
+    // created an array to store the type objects.
+    let pokemonTypes = [];
+    Object.keys(pokemon.types).forEach((key) => {
+      pokemonTypes.push(pokemon.types[key].type.name);
+    });
+
+    let typesElement = document.createElement('p');
+    typesElement.innerText = 'Types: ' + pokemonTypes;
+
+    let imageElement = document.createElement('img');
+    imageElement.classList.add('pokeImage');
+    imageElement.src = pokemon.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(typesElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  // create load function to fetch data fron API
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -65,7 +124,7 @@ let pokemonRepository = (function () {
     })
   }
 
-//create load detals function
+  //create load detals function
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
